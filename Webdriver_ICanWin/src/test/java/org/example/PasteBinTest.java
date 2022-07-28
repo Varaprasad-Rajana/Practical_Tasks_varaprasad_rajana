ab6af542903564942fae6394d84d6f8b60dfc96d
Varaprasad_Rajana <Varaprasad_Rajana@epam.com>
2022-07-28 14:05:10 +0530

    finished webdriver module mandatory task I can Win

---
+++ Webdriver_ICanWin/src/test/java/org/example/PasteBinTest.java
@@ -0,0 +1,26 @@
+package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class PasteBinTest {
+
+    @Test
+    public  void pasteBin() {
+        //System.setProperty("webdriver.chrome.driver","C:\Program Files\Selenium-drivers\chromedriver_win32\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium-drivers\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://pastebin.com/");
+        driver.findElement(By.id("postform-text")).sendKeys("Hello from WebDriver");
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        javascriptExecutor.executeScript("window.scrollBy(0,360)","");
+        driver.findElement(By.xpath("//*[@id=\"select2-postform-expiration-container\"]")).click();
+        driver.findElement(By.xpath("//span[@class='select2-results']/ul/li[3]")).click();
+        javascriptExecutor.executeScript("window.scrollBy(0,360)","");
+        driver.findElement(By.id("postform-name")).sendKeys("helloweb");
+        driver.findElement(By.tagName("button")).click();
+    }
+}
