de0211f139cbc492b455928445d7b5f499460427
Varaprasad_Rajana <Varaprasad_Rajana@epam.com>
2022-08-10 12:53:41 +0530

    solved

---
+++ Jenkins_Integration/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello CI!" );
+    }
+
+    public static void doNothing() {
+        // do nothing
+    }
+}}
